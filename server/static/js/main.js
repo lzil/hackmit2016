d69eb23b9a1803339db16191afd870905ac8e3cf
Menghua Wu <rmwu@mit.edu>
2016-09-17 19:42:24 -0400

    uploading normal-sized stuff seems ok

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -1,5 +1,32 @@
-$(function() {
+$(function()
+{    
+    var fileInput = $("#upload");
+    
+    $(fileInput).on("change", function()
+    {        
+        // actual file input stuff
+        var fileInputVal = $(fileInput)[0].files[0];
+        
+        if (fileInputVal != ""){
+            var reader = new FileReader();
+            reader.readAsDataURL(fileInputVal);
+            
+            reader.onload = function()
+            {
+                var formData = new FormData();
+            
+                formData.append("file", reader.result);
+                formData.append("filename", fileInputVal.name);
 
+                var http = new XMLHttpRequest();
+                http.open("POST", "/upload");
+                http.send(formData);
+
+                // reset input
+                $(fileInput)[0].files = null;
+            }
+        }
+    });
 
 });
 
--- server/views.py
+++ server/views.py
@@ -9,13 +9,19 @@
     :license: lol don't steal this code pls
 """
 
-from flask import render_template, request
-
+import os
+from flask import render_template, request, jsonify
 from server import app, HOSTNAME
+
+import base64
+import cStringIO
+from PIL import Image
 
 # **********************************************************
 # FILE UPLOADING
 # **********************************************************
+
+UPLOAD_FOLDER = "uploads"
 
 # separates file extension and checks if is an image file
 ALLOWED_EXTENSIONS = set(['png','tiff','tif','jpg','jpeg','JPG','TIFF','TIF','PNG'])
@@ -23,13 +29,37 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+# b64: base64 encoded string
+# return: decoded version of b64 with tag removed
+def decodeB64(b64):
+    str = b64.replace('data:image/png;base64,', '') #replace tag
+    decoded = base64.b64decode(str)
+    return decoded
+
+# imgData: raw image data in string form
+# return: PIL object containing the image, or None
+def encodePIL(imgData):
+    try:
+        str = cStringIO.StringIO()              # PIL only accepts weird formats
+        str.write(imgData)                      # so make it a cStringIO
+        img = Image.open(str)                   # open Image by str data
+        return img                              # a pickled image? how sour
+    except:
+        return None;
+
 # file uploading backend
 @app.route("/upload", methods=['POST'])
 def upload():
-    file = request.files['file']
-    if file and allowed_file(file.filename):
-        pass
-        
+    fileObj = request.form['file']
+    filename = request.form['filename']
+
+    if fileObj and allowed_file(filename):
+        fileDec = decodeB64(fileObj)
+        fileImg = encodePIL(fileDec)
+
+        fullpath = os.path.join(UPLOAD_FOLDER, filename)
+        fileImg.save(fullpath)
+
     num = 0
     return jsonify(score=num)
 
