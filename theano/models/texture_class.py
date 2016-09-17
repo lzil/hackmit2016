5cdd6503fdb79ea1bb72ace78b6d44644376a72a
lzil <zhoul@mit.edu>
2016-09-17 19:37:04 -0400

    texture edits

--- theano/models/texture_class.py
+++ theano/models/texture_class.py
@@ -19,89 +19,11 @@ from theano.tensor.nnet import conv2d
 
 from logistic_sgd import LogisticRegression, load_data
 from mlp import HiddenLayer
+from conv import LeNetConvPoolLayer
 
-
-class LeNetConvPoolLayer(object):
-	"""Pool Layer of a convolutional network """
-
-	def __init__(self, rng, input, filter_shape, image_shape, poolsize=(2, 2)):
-		"""
-		Allocate a LeNetConvPoolLayer with shared variable internal parameters.
-
-		:type rng: numpy.random.RandomState
-		:param rng: a random number generator used to initialize weights
-
-		:type input: theano.tensor.dtensor4
-		:param input: symbolic image tensor, of shape image_shape
-
-		:type filter_shape: tuple or list of length 4
-		:param filter_shape: (number of filters, num input feature maps,
-							  filter height, filter width)
-
-		:type image_shape: tuple or list of length 4
-		:param image_shape: (batch size, num input feature maps,
-							 image height, image width)
-
-		:type poolsize: tuple or list of length 2
-		:param poolsize: the downsampling (pooling) factor (#rows, #cols)
-		"""
-
-		assert image_shape[1] == filter_shape[1]
-		self.input = input
-
-		# there are "num input feature maps * filter height * filter width"
-		# inputs to each hidden unit
-		fan_in = numpy.prod(filter_shape[1:])
-		# each unit in the lower layer receives a gradient from:
-		# "num output feature maps * filter height * filter width" /
-		#   pooling size
-		fan_out = (filter_shape[0] * numpy.prod(filter_shape[2:]) //
-				   numpy.prod(poolsize))
-		# initialize weights with random weights
-		W_bound = numpy.sqrt(6. / (fan_in + fan_out))
-		self.W = theano.shared(
-			numpy.asarray(
-				rng.uniform(low=-W_bound, high=W_bound, size=filter_shape),
-				dtype=theano.config.floatX
-			),
-			borrow=True
-		)
-
-		# the bias is a 1D tensor -- one bias per output feature map
-		b_values = numpy.zeros((filter_shape[0],), dtype=theano.config.floatX)
-		self.b = theano.shared(value=b_values, borrow=True)
-
-		# convolve input feature maps with filters
-		conv_out = conv2d(
-			input=input,
-			filters=self.W,
-			filter_shape=filter_shape,
-			input_shape=image_shape
-		)
-
-		# pool each feature map individually, using maxpooling
-		pooled_out = pool.pool_2d(
-			input=conv_out,
-			ds=poolsize,
-			ignore_border=True
-		)
-
-		# add the bias term. Since the bias is a vector (1D array), we first
-		# reshape it to a tensor of shape (1, n_filters, 1, 1). Each bias will
-		# thus be broadcasted across mini-batches and feature map
-		# width & height
-		self.output = T.tanh(pooled_out + self.b.dimshuffle('x', 0, 'x', 'x'))
-
-		# store parameters of this layer
-		self.params = [self.W, self.b]
-
-		# keep track of model input
-		self.input = input
-
-
-def evaluate_lenet5(learning_rate=0.1, n_epochs=200,
+def evaluate_image(learning_rate=0.1, n_epochs=50,
 					dataset='mnist.pkl.gz',
-					nkerns=[20, 50], batch_size=500):
+					nkerns=[10, 15], batch_size=20):
 	""" Demonstrates lenet on MNIST dataset
 
 	:type learning_rate: float
@@ -159,8 +81,8 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=200,
 	layer0 = LeNetConvPoolLayer(
 		rng,
 		input=layer0_input,
-		image_shape=(batch_size, 1, 28, 28),
-		filter_shape=(nkerns[0], 1, 5, 5),
+		image_shape=(batch_size, 1, 128, 128),
+		filter_shape=(nkerns[0], 1, 9, 9),
 		poolsize=(2, 2)
 	)
 
@@ -169,6 +91,14 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=200,
 	# maxpooling reduces this further to (8/2, 8/2) = (4, 4)
 	# 4D output tensor is thus of shape (batch_size, nkerns[1], 4, 4)
 	layer1 = LeNetConvPoolLayer(
+		rng,
+		input=layer0.output,
+		image_shape=(batch_size, nkerns[0], 60, 60),
+		filter_shape=(nkerns[1], nkerns[0], 5, 5),
+		poolsize=(2, 2)
+	)
+
+	layer1b = LeNetConvPoolLayer(
 		rng,
 		input=layer0.output,
 		image_shape=(batch_size, nkerns[0], 12, 12),
@@ -180,19 +110,19 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=200,
 	# shape (batch_size, num_pixels) (i.e matrix of rasterized images).
 	# This will generate a matrix of shape (batch_size, nkerns[1] * 4 * 4),
 	# or (500, 50 * 4 * 4) = (500, 800) with the default values.
-	layer2_input = layer1.output.flatten(2)
+	layer2_input = layer1b.output.flatten(2)
 
 	# construct a fully-connected sigmoidal layer
 	layer2 = HiddenLayer(
 		rng,
 		input=layer2_input,
-		n_in=nkerns[1] * 4 * 4,
-		n_out=500,
+		n_in=nkerns[1] * 12 * 12,
+		n_out=50,
 		activation=T.tanh
 	)
 
 	# classify the values of the fully-connected sigmoidal layer
-	layer3 = LogisticRegression(input=layer2.output, n_in=500, n_out=10)
+	layer3 = LogisticRegression(input=layer2.output, n_in=100, n_out=1)
 
 	# the cost we minimize during training is the NLL of the model
 	cost = layer3.negative_log_likelihood(y)
@@ -248,10 +178,10 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=200,
 	###############
 	print('... training')
 	# early-stopping parameters
-	patience = 10000  # look as this many examples regardless
+	patience = 300  # look as this many examples regardless
 	patience_increase = 2  # wait this much longer when a new best is
 						   # found
-	improvement_threshold = 0.995  # a relative improvement of this much is
+	improvement_threshold = 0.95  # a relative improvement of this much is
 								   # considered significant
 	validation_frequency = min(n_train_batches, patience // 2)
 								  # go through this many
@@ -286,7 +216,6 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=200,
 				print('epoch %i, minibatch %i/%i, validation error %f %%' %
 					  (epoch, minibatch_index + 1, n_train_batches,
 					   this_validation_loss * 100.))
-
 				# if we got the best validation score until now
 				if this_validation_loss < best_validation_loss:
 
@@ -324,8 +253,8 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=200,
 		   ' ran for %.2fm' % ((end_time - start_time) / 60.)), file=sys.stderr)
 
 if __name__ == '__main__':
-	evaluate_lenet5()
+	evaluate_image()
 
 
 def experiment(state, channel):
-	evaluate_lenet5(state.learning_rate, dataset=state.dataset)
+	evaluate_image(state.learning_rate, dataset=state.dataset)
