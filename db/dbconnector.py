a8c01707ceb7d200cd71b06642c59d6a26a166c3
Menghua Wu <rmwu@mit.edu>
2016-09-17 16:16:59 -0400

    rm

--- db/dbconnector.py
+++ db/dbconnector.py
@@ -22,9 +22,7 @@ class DecimalEncoder(json.JSONEncoder):
 # connection manager
 dynamodb = boto3.resource('dynamodb',
         region_name='us-east-1',
-        endpoint_url="https://dynamodb.us-east-1.amazonaws.com",
-        aws_secret_access_key="hI37+gaZ7+Sbpd1LxjfuY2INRquMX7AXQP/arFH8",
-        aws_access_key_id="AKIAJNLHPPFJ2AJ7ZCOQ")
+        endpoint_url="https://dynamodb.us-east-1.amazonaws.com")
 
 table = dynamodb.Table(db_name)
 
