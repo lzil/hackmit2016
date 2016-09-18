accc22a307523ea4a5f261ecd10f596c06b0819e
graceyin1218 <graceyin1218@gmail.com>
2016-09-18 02:39:46 -0400

    cleanup

--- scraper/download.py
+++ scraper/download.py
@@ -11,8 +11,6 @@ def download(searchTerm):
   # find the file and go through each line (each url)
   lines = [line.rstrip('\n') for line in open('../cache/links/' + searchTerm + ".txt")]
 
-  # create the directory if necessary
-
   # download the image and save it in cache/tmp/<searchTerm>
   num = 0
   for line in lines:
@@ -28,15 +26,4 @@ def download(searchTerm):
     print(num)
     print(filename)
     num += 1
-
-    """
-    filename = str(num)
-    file = open("../cache/tmp/" + searchTerm + "/" + filename + ".jpg")
-    urllib.urlretrieve(line, "../cache/tmp/" + searchTerm + "/" + filename + ".jpg")
-    num += 1
-    file.close()
-    """
-
-download("green")
-
 
