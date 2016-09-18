7245daeed292277bc089df556684dcbfc14e98e5
Menghua Wu <rmwu@mit.edu>
2016-09-17 23:43:49 -0400

    want to pull

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -1,34 +1,96 @@
 $(function()
-{    
-    var fileInput = $("#upload");
+{
+    // aesthetics
+    $(".header a img").on("mouseover", function(){
+        $(this).attr("src", "/static/img/logo.png");
+    });
+    $(".header a img").on("mouseout", function(){
+        $(this).attr("src", "/static/img/logo2.png");
+    });
     
+    var fileInput = $("#upload");
+
     $(fileInput).on("change", function()
-    {        
-        // actual file input stuff
-        var fileInputVal = $(fileInput)[0].files[0];
-        
-        if (fileInputVal != ""){
-            var reader = new FileReader();
-            reader.readAsDataURL(fileInputVal);
-            
-            reader.onload = function()
-            {
-                var formData = new FormData();
-            
-                formData.append("file", reader.result);
-                formData.append("filename", fileInputVal.name);
-
-                var http = new XMLHttpRequest();
-                http.open("POST", "/upload");
-                http.send(formData);
-
-                // reset input
-                $(fileInput)[0].files = null;
-            }
-        }
+    {
+        sendRequest();
     });
 
+    $("#queryID").on("focusout", function()
+    {
+        sendRequest();
+    });
 });
+
+function sendRequest()
+{
+    var fileInput = $("#upload");
+    // actual file input stuff
+    var fileInputVal = $(fileInput)[0].files[0];
+    // query string
+    var searchID = $("#queryID").val();
+    
+    if (fileInputVal != "" && searchID != "")
+    {
+        var reader = new FileReader();
+
+        reader.onloadend = function()
+        {
+            var formData = new FormData();
+
+            formData.append("file", reader.result);
+            formData.append("filename", fileInputVal.name);
+            formData.append("searchID", searchID);
+
+            var http = new XMLHttpRequest();
+            http.open("POST", "/upload");
+            http.send(formData);
+
+            // reset input
+            $(fileInput)[0].files = null;
+        }
+        
+        reader.readAsDataURL(fileInputVal);
+    }
+}
+
+function previewImg(img)
+{
+    var canvas = document.getElementsByTagName("canvas")[0];
+    var holder = $("#box");
+    
+    // resize img so that longest size is at max, 256
+    var MAX_WIDTH = 256;
+    var MAX_HEIGHT = 256;
+    var width = img.width;
+    var height = img.height;
+
+    if (width > height)
+    {
+        if (width > MAX_WIDTH)
+        {
+            height *= MAX_WIDTH / width;
+            width = MAX_WIDTH;
+        }
+    }
+    else
+    {
+        if (height > MAX_HEIGHT)
+        {
+            width *= MAX_HEIGHT / height;
+            height = MAX_HEIGHT;
+        }
+    }
+
+    canvas.width = width;
+    canvas.height = height;
+
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0, width, height);
+
+    var dataurl = canvas.toDataURL();
+    
+    return dataurl;
+}
 
 function previewFile()
 {
@@ -39,6 +101,7 @@ function previewFile()
     reader.onloadend = function()
     {
         preview.src = reader.result;
+        previewImg(preview);
     }
 
     if (file)
@@ -47,11 +110,6 @@ function previewFile()
     }
     else
     {
-        preview.src = "";
+        // preview.src = "";
     }
-
-    $("#preview").css(
-    {
-        opacity: 1
-    });
 }
