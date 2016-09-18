dfff24579235ad5773ec69361eaa9a9700cdd813
Tony Zhang <txz@mit.edu>
2016-09-17 23:14:39 -0400

    datasets now flattened

--- theano/models/get_data.py
+++ theano/models/get_data.py
@@ -14,15 +14,15 @@ def directory_to_dataset(dirname, permute=True):
     files = os.listdir(dirname)
     if permute:
         random.shuffle(files)
-    all_data = [image_to_ndarray(os.path.join(dirname, path)) for path in files]
+    all_data = [image_to_ndarray(os.path.join(dirname, path)).flatten() for path in files]
     num_data_points = len(all_data)
 
     seventy_percent = num_data_points * 70 // 100
     eightyfive_percent = num_data_points * 85 // 100
 
-    return (all_data[:seventy_percent],
-            all_data[seventy_percent:eightyfive_percent],
-            all_data[eightyfive_percent:])
+    return (numpy.asarray(all_data[:seventy_percent]),
+            numpy.asarray(all_data[seventy_percent:eightyfive_percent]),
+            numpy.asarray(all_data[eightyfive_percent:]))
 
 
 def image_to_ndarray(path):
