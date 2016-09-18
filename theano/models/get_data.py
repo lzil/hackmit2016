f86fc2d34ac53b41cf7b3f2f844713cffa89e34d
Tony Zhang <txz@mit.edu>
2016-09-17 23:39:59 -0400

    minor refactor

--- theano/models/get_data.py
+++ theano/models/get_data.py
@@ -10,19 +10,26 @@ CACHE_DIR = "../../cache/links/"
 
 
 def directory_to_dataset(dirname, permute=True):
+    # check precondition
     assert(os.path.isdir(dirname))
+
     files = os.listdir(dirname)
     if permute:
         random.shuffle(files)
     all_data = [image_to_ndarray(os.path.join(dirname, path)).flatten() for path in files]
     num_data_points = len(all_data)
 
-    seventy_percent = num_data_points * 70 // 100
-    eightyfive_percent = num_data_points * 85 // 100
+    train_frac = num_data_points * 70 // 100
+    non_test_frac = num_data_points * 85 // 100
 
-    return (numpy.asarray(all_data[:seventy_percent]),
-            numpy.asarray(all_data[seventy_percent:eightyfive_percent]),
-            numpy.asarray(all_data[eightyfive_percent:]))
+    # sanity checks
+    assert(train_frac < non_test_frac)
+    assert(non_test_frac < num_data_points)
+
+    # train 70, validate 15, test 15
+    return (numpy.asarray(all_data[:train_frac]),
+            numpy.asarray(all_data[train_frac:non_test_frac]),
+            numpy.asarray(all_data[non_test_frac:]))
 
 
 def image_to_ndarray(path):
